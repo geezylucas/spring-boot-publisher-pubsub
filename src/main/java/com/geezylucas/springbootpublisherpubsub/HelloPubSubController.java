0faee2a413d04dfcee76f93def170960ee28fb8e
Gerardo Lucas Cruz <sorec@live.com>
2024-09-24 01:05:14 -0600

    Initial project

---
+++ src/main/java/com/geezylucas/springbootpublisherpubsub/HelloPubSubController.java
@@ -0,0 +1,34 @@
+package com.geezylucas.springbootpublisherpubsub;
+
+import com.geezylucas.springbootpublisherpubsub.dto.UserMessageDTO;
+import com.geezylucas.springbootpublisherpubsub.model.UserMessage;
+import com.geezylucas.springbootpublisherpubsub.publishers.HelloPubSubPublisher;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+public class HelloPubSubController {
+
+    private final HelloPubSubPublisher publisher;
+
+    @PostMapping("/publish")
+    public Mono<String> publish(@RequestBody UserMessageDTO userMessageDTO) {
+        log.info("Received a POST at /v1/publish with message: {}", userMessageDTO);
+
+        UserMessage userMessage = new UserMessage(userMessageDTO.body(), userMessageDTO.username(), LocalDateTime.now());
+
+        publisher.publish(userMessage);
+
+        return Mono.just("Message published");
+    }
+}
---
+++ src/main/java/com/geezylucas/springbootpublisherpubsub/SpringBootPublisherPubsubApplication.java
@@ -0,0 +1,13 @@
+package com.geezylucas.springbootpublisherpubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPublisherPubsubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPublisherPubsubApplication.class, args);
+    }
+
+}
